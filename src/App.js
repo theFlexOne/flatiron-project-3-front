97c85e9803c4ce4772039e89cbeee973836add77
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-03-17 16:53:06 -0500

    built App component with Header & Main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import { AppProvider } from "./contexts/AppContext";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* MAIN HERE */}
+    <div className="app">
+      <AppProvider>
+        <Header />
+        <Main />
+      </AppProvider>
     </div>
   );
 }
