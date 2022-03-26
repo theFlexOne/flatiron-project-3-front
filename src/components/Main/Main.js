af3126063af55b12831f6be0230207d1a7938401
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-03-26 13:10:07 -0500

    created Main component as a global container for all main content

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main.css";
+import AppRouter from "../../routers/AppRouter";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <AppRouter />
+    </div>
+  );
+};
+
+export default Main;
