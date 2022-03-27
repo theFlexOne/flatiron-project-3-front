f7ef597b07ddd05125aa5ab8d6a78dd153bf2b0c
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-03-27 16:07:21 -0500

    deleted AppRouter.js file and moved contents into Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./main.css";
-import AppRouter from "../../routers/AppRouter";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Music from "../Music/Music";
+import { MusicProvider } from "../../context/MusicContext";
+import Playlists from "../Playlists/Playlists";
 
 const Main = () => {
   return (
     <div className="main">
-      <AppRouter />
+      <Routes>
+        <Route path="/" element={<Navigate to="/music" />} />
+        <Route
+          path="/music/*"
+          element={
+            <MusicProvider>
+              <Music />
+            </MusicProvider>
+          }
+        >
+          <Route path="playlists" element={<Playlists />} />
+          {/* nest a :id route inside playlists? */}
+        </Route>
+      </Routes>
     </div>
   );
 };
