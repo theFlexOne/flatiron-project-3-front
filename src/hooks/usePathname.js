f732bb9b77141740d5d16ccd283180863e1dc717
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-11 02:20:31 -0500

    created custom hook usePathname

---
+++ src/hooks/usePathname.js
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+
+const PATHNAME_SEGMENTS_REGEXP = /(?<=\/)[^\/?]+/g;
+
+const usePathname = () => {
+  const { pathname } = useLocation();
+  const matches = [...pathname.matchAll(PATHNAME_SEGMENTS_REGEXP)];
+  const pathSegments = matches.map((match) => match[0]);
+
+  const rootPath = pathSegments[0];
+  const currentPath = pathSegments.slice(-1)[0];
+  const parentPath = pathSegments.slice(-2)[0] || "";
+
+  const pathObj = {
+    pathname,
+    pathSegments,
+    rootPath,
+    parentPath,
+    currentPath,
+    first: rootPath,
+    last: currentPath,
+    toString: () => pathname,
+  };
+
+  pathSegments.forEach((segment, i) => (pathObj[i] = segment));
+
+  return pathObj;
+};
+
+export default usePathname;
